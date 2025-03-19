c63313702f28e0560baeb7f339db8ba26e5c9402
Karim CHARLEUX <karim.charleux@hotmail.fr>
2025-03-19 09:33:46 +0100

    Add new event

--- app/data/evenementsData.tsx
+++ app/data/evenementsData.tsx
@@ -18,7 +18,7 @@ const evenementsData: Evenement[] = [
     imageSrc: '/events/bal-des-fous.jpg',
     categorie: 'Festival',
     dossierImages: '/events/bal-des-fous/',
-    date: '10 août 2023',
+    date: '10 janvier 2025',
     lieu: 'Paris, France',
     type: 'selection',
     prixParPhoto: 4,
@@ -30,11 +30,23 @@ const evenementsData: Evenement[] = [
     imageSrc: '/events/birthday-event.jpg',
     categorie: 'Anniversaire',
     dossierImages: '/events/birthday-event/',
-    date: '10 août 2023',
+    date: '28 février 2025',
     lieu: 'Paris, France',
     type: 'selection',
     prixParPhoto: 4,
-    images: ["1.jpg", "2.jpg"]
+    images: ["10.jpg", "11.jpg", "12.jpg"]
+  },
+  {
+    id: 'mouratoglou',
+    titre: 'Mouratoglou',
+    imageSrc: '/events/mouratoglou.jpg',
+    categorie: 'Soirée',
+    dossierImages: '/events/mouratoglou/',
+    date: '1 mars 2025',
+    lieu: 'Paris, France',
+    type: 'selection',
+    prixParPhoto: 4,
+    images: ["3.jpg", "4.jpg"]
   }
 ];
 
